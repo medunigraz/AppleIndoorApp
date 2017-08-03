a8b16958fbc2f2d4730576e3728e5ca9bd83c3c3
gze <gze@gzes-Mac-mini.local>
2017-08-03 09:28:05 +0200

    Converts API Date

---
+++ MedCampusNaviWithExtras/Util.swift
@@ -0,0 +1,17 @@
+//
+//  Util.swift
+//  MedCampusNaviWithExtras
+//
+//  Created by gze on 03.08.17.
+//  Copyright © 2017 mug. All rights reserved.
+//
+
+import Foundation
+
+class Util{
+    //Converts Dates from Api
+    func convertDate(date: String) ->String {
+        let index: Range<String.Index> = date.range(of: "T")!
+        return date.substring(to: index.lowerBound)
+    }
+}
