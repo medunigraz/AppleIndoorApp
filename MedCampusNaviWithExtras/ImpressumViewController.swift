83162a37b834c0c1500d5159136c57ac003008ec
gze <gze@gzes-Mac-mini.local>
2017-08-03 07:52:41 +0200

    new design

---
+++ MedCampusNaviWithExtras/ImpressumViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ImpressumViewController.swift
+//  MedCampusNaviWithExtras
+//
+//  Created by gze on 02.08.17.
+//  Copyright © 2017 mug. All rights reserved.
+//
+
+import UIKit
+
+class ImpressumViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
